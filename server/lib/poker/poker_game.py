9958c3aed0003e4c9abf466e8c21a1d281aed992
YSchilling <78358953+YSchilling@users.noreply.github.com>
2023-11-01 13:31:49 +0100

    added fastapi and demo player list

--- server/lib/poker/poker_game.py
+++ server/lib/poker/poker_game.py
@@ -4,7 +4,7 @@ from .game_config import GameConfig
 from .player_manager import PlayerManager
 from .enums import GameState
 from .player import Player
-import sys
+
 
 class PokerGame:
     def __init__(self, config: GameConfig) -> None:
@@ -13,20 +13,21 @@ class PokerGame:
         self.player_manager = PlayerManager()
         self.card_manager = CardManager()
         self.chip_manager = ChipManager(config)
-    
+
     def start(self) -> None:
         self.state = GameState.RUNNING
-        self.chip_manager.give_players_money(self.player_manager.players, self.config.player_starting_money)
+        self.chip_manager.give_players_money(
+            self.player_manager.players, self.config.player_starting_money)
         self.chip_manager.give_players_buttons()
         self.start_round()
 
     def start_round(self) -> None:
         self.card_manager.deck.shuffle()
         self.card_manager.give_players_cards(self.player_manager.players)
-    
+
     def join_player(self, player: Player) -> None:
         if self.state != GameState.CREATING:
-            print("Player can't join while game is not in creating state!")
-            sys.exit(0)
-        
+            raise ValueError(
+                "Player can't join while game is not in creating state!")
+
         self.player_manager.players.append(player)
--- server/main.py
+++ server/main.py
@@ -1,51 +1,22 @@
-from lib.poker.game_config import GameConfig
+from fastapi import FastAPI
 from lib.poker.poker_game import PokerGame
 from lib.poker.player import Player
-from lib.playing_cards.card import Card
-from lib.playing_cards.enums import CardColor
-from lib.playing_cards.enums import CardValue
-from lib.poker.helper import calculate_player_best_hand
+
+app = FastAPI()
+
+players: list[player] = []
 
 
-def main():
-    '''
-    # create game
-    config = GameConfig(1000, 2, 50)
-    game = PokerGame(config)
-
-    # add players
-    for i in range(1, 5):
-        player = Player(f"Player {i}")
-        game.join_player(player)
-
-    for player in game.player_manager.players:
-        print(player)
-
-    # start round
-    print(game.card_manager.deck)
-    game.start_round()
-    print(game.card_manager.deck)
-
-    for player in game.player_manager.players:
-        print(player)
-    '''
-
-    player = Player("kek")
-    player.cards = [
-        Card(CardColor.HEARTS, CardValue.TWO),
-        Card(CardColor.HEARTS, CardValue.THREE)
-    ]
-
-    community_cards = [
-        Card(CardColor.CLUBS, CardValue.NINE),
-        Card(CardColor.HEARTS, CardValue.SIX),
-        Card(CardColor.HEARTS, CardValue.KING),
-        Card(CardColor.CLUBS, CardValue.ACE),
-        Card(CardColor.CLUBS, CardValue.TEN),
-    ]
-
-    print(calculate_player_best_hand(player, community_cards))
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
-if __name__ == "__main__":
-    main()
+@app.post("/add-player/{player_name}")
+async def add_player(name: str):
+    players.append(Player(name))
+
+
+@app.get("get-players")
+async def get_players() -> list[Player]:
+    return players
