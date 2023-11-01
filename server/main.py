ee9f96e4376a8fa7052b4d18ba199cd93eefa273
YSchilling <78358953+YSchilling@users.noreply.github.com>
2023-11-01 13:56:18 +0100

    simple player list

--- server/main.py
+++ server/main.py
@@ -1,22 +1,16 @@
 from fastapi import FastAPI
-from lib.poker.poker_game import PokerGame
-from lib.poker.player import Player
 
 app = FastAPI()
 
-players: list[player] = []
+players: list[str] = []
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.post("/add-player/{player_name}")
+@app.get("/add-player/{name}")
 async def add_player(name: str):
-    players.append(Player(name))
+    players.append(name)
+    return "OK"
 
 
-@app.get("get-players")
-async def get_players() -> list[Player]:
+@app.get("/get-players")
+async def get_players():
     return players
