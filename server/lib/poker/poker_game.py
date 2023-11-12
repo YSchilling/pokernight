ca2f40165c203b3b8aa37a0d552e85f653f71ba0
YSchilling <78358953+YSchilling@users.noreply.github.com>
2023-11-12 13:26:47 +0100

    created round flow

--- server/lib/poker/poker_game.py
+++ server/lib/poker/poker_game.py
@@ -1,9 +1,17 @@
 from .player import Player
+from .player import PlayerAction
 from ..playing_cards.deck import Deck
 from ..playing_cards.card import Card
 from .calculate_winner import PokerHand
 from .calculate_winner import calculate_winner
 
+import enum
+
+class RoundPhase(enum.Enum):
+    PREFLOP = enum.auto()
+    FLOP = enum.auto()
+    TURN = enum.auto()
+    RIVER = enum.auto()
 
 class PokerGame:
     def __init__(self):
@@ -13,20 +21,55 @@ class PokerGame:
         self.community_cards: list[Card] = []
 
     def run(self):
-        # shuffle deck
-        self.deck.shuffle()
+        self._start_round()
 
-        # give players cards
-        for _ in range(2):
+    def _start_round(self):
+        self._prepare_round()
+        self._play_round()
+        self._end_round()
+
+    def _prepare_round(self):
+        self.deck.shuffle()
+        self._give_players_cards()
+    
+    def _give_players_cards(self):
+        CARD_COUNT = 2
+        for _ in range(CARD_COUNT):
             for player in self.players:
                 player.cards.append(self.deck.draw_card())
-        # deal community cards
-        for _ in range(5):
-            self.community_cards.append(self.deck.draw_card())
-        # print game state
+    
+    def _play_round(self):
+        for round_phase in RoundPhase:
+            self._deal_community_cards(round_phase)
+            print("PLAYER ACTIONS")
+    
+    def _deal_community_cards(self, round_phase: RoundPhase) -> None:
+        match round_phase:
+            case RoundPhase.PREFLOP:
+                print("PREFLOP")
+            case RoundPhase.FLOP:
+                print("FLOP")
+                self.community_cards.append(self.deck.draw_card())
+                self.community_cards.append(self.deck.draw_card())
+                self.community_cards.append(self.deck.draw_card())
+            case RoundPhase.TURN:
+                print("TURN")
+                self.community_cards.append(self.deck.draw_card())
+            case RoundPhase.RIVER:
+                print("RIVER")
+                self.community_cards.append(self.deck.draw_card())
+
+    def _end_round(self):
         winners = calculate_winner(self.players, self.community_cards)
         if len(winners) == 1:
             print(winners[0].name)
         else:
             for player in winners:
                 print(player.name)
+
+        self._reset_round()
+
+    def _reset_round(self):
+        self.deck = Deck()
+        for player in self.players:
+            player.cards = []
